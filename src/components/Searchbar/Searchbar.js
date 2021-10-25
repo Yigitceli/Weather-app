52feff9cd194453bbe6663dcf5f55ee0ab541eef
Yigitceli <Yigit.celi@outlook.com>
2021-10-26 01:09:03 +0300

    Link reconfig

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,5 +1,5 @@
 import { SearchIcon } from "@chakra-ui/icons";
-import { Flex, Link } from "@chakra-ui/layout";
+import { Flex } from "@chakra-ui/layout";
 import {
   FormControl,
   Input,
@@ -11,6 +11,7 @@ import React, { useState } from "react";
 import { useHistory } from "react-router";
 import { useColorMode, useColorModeValue } from "@chakra-ui/color-mode";
 import { AiOutlineHome, AiFillHome } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 export default function Searchbar() {
   const bg = useColorModeValue("#1a202c", "#9fafca");
@@ -28,14 +29,14 @@ export default function Searchbar() {
 
   return (
     <Flex flexDirection="column" alignItems="center">
-      <IconButton
-        href="/"
-        as={Link}
-        bg={bg}
-        color={bg2}
-        w="8em"
-        icon={colorMode === "light" ? <AiOutlineHome /> : <AiFillHome />}
-      ></IconButton>
+      <Link to="/">
+        <IconButton
+          bg={bg}
+          color={bg2}
+          w="8em"
+          icon={colorMode === "light" ? <AiOutlineHome /> : <AiFillHome />}
+        ></IconButton>
+      </Link>
 
       <FormControl w={["100%", "80%", "60%", "40%"]} py="1em">
         <form onSubmit={submitHandler}>
